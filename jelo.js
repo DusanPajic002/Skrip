83d94fa7f79a746a9e5e4f7e0a2ae96d0853d4da
DusanPajic002 <dusanpajic2002@raf.rs>
2023-10-17 14:56:59 +0200

    123

--- jelo.js
+++ jelo.js
@@ -4,8 +4,15 @@ window.addEventListener("load", function(){
     //tj DOM tree potpuno formiranim
 });
 
-document.getElementById("forma").addEventListener("submit", function validacija(){
-    var validno = true;
+document.getElementById("forma").addEventListener("submit", function validacija(e){
+    let validno = true;
+
+    let spanovi = document.querySelectorAll("#sastojci > span.badge");
+    let niz = [];
+        for(let i=0; i<spanovi.length; i++){
+        niz.push(spanovi[i].dataset.id);
+    }
+
     if( document.getElementById("naziv").value.length < 3 ){
         validno = false;
         document.getElementById("naziv").classList.add("error");
@@ -15,6 +22,8 @@ document.getElementById("forma").addEventListener("submit", function validacija(
         document.getElementById("naziv").classList.add("success");
         document.getElementById("naziv").classList.remove("error");
     }
+    if (!validno)
+        e.preventDefault();
     return validno;
 });
 
@@ -24,40 +33,34 @@ document.getElementById("naziv").addEventListener("keypress", function(){
 });
 
 document.getElementById("dodaj-sastojak").addEventListener("click", function(){
-    var id = document.getElementById("spisak-sastojaka").value;
+    let id = document.getElementById("spisak-sastojaka").value;
     if(!id){
         alert("Izaberi sastojak");
         return;
     }
     dodajSastojak(id);
-    var spanovi = document.querySelectorAll("#sastojci > span.badge");
-    var niz = [];
-        for(let i=0; i<spanovi.length; i++){
-        niz.push(spanovi[i].dataset.id);
-    }
 
-    
 });
 function dodajSastojak(id){
-    document.querySelector(`#spisak-sastojaka > option[value='${id}']`).disabled = true;
-    document.getElementById("spisak-sastojaka").selectedIndex = 0;
-    var naziv = document.querySelector(`#spisak-sastojaka > option[value='${id}']`).innerHTML;
-    var span = document.createElement("span");
-    var button = document.createElement("button");
-    button.type="button";
-    button.classList.add("btn");
-    button.classList.add("btn-default");
-    button.classList.add("btn-sm");
-    button.innerHTML = "X";
-    span.classList.add("badge");
-    span.classList.add("bg-secondary");
-    span.dataset.id = id;
-    span.innerHTML = naziv;
-    span.appendChild(button);
-    document.getElementById("sastojci").appendChild(span);
-    document.getElementById("sastojci").appendChild(document.createTextNode(" "));
-    button.addEventListener("click", function(){   
-        var id = this.parentNode.dataset.id;
+        document.querySelector(`#spisak-sastojaka > option[value='${id}']`).disabled = true;
+        document.getElementById("spisak-sastojaka").selectedIndex = 0;
+        let naziv = document.querySelector(`#spisak-sastojaka > option[value='${id}']`).innerHTML;
+        let span = document.createElement("span");
+        let button = document.createElement("button");
+        button.type="button";
+        button.classList.add("btn");
+        button.classList.add("btn-default");
+        button.classList.add("btn-sm");
+        button.innerHTML = "X";
+        span.classList.add("badge");
+        span.classList.add("bg-secondary");
+        span.dataset.id = id;
+        span.innerHTML = naziv;
+        span.appendChild(button);
+        document.getElementById("sastojci").appendChild(span);
+        document.getElementById("sastojci").appendChild(document.createTextNode(" "));
+        button.addEventListener("click", function(){   
+        let id = this.parentNode.dataset.id;
         this.parentNode.parentNode.removeChild( this.parentNode );
         document.querySelector(`#spisak-sastojaka > option[value='${id}']`).disabled = false;
      });
