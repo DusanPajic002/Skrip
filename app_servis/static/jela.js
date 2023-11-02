1416f0ce71d168467b087b2604d0df14273717af
DusanPajic002 <dusanpajic2002@gmail.com>
2023-11-02 11:56:33 +0100

    1

--- app_servis/static/jela.js
+++ app_servis/static/jela.js
@@ -1,8 +1,8 @@
 function oreditip(i) {
     if (i == 1)
-        return "Sendvic";
+        return "Pica";
     else if (i == 2)
-        return "Kapricoza";
+        return "Sendvic";
 }
 
 window.addEventListener("load", function () {
@@ -12,41 +12,32 @@ window.addEventListener("load", function () {
 
             for (let i = 0; i < data.length; i++) {
                 let tr = document.createElement("tr");
-                let td_slika = document.createElement("td");
-                let td_model = document.createElement("td");
+                let td_kategorija = document.createElement("td");
                 let td_opis = document.createElement("td");
                 let td_cena = document.createElement("td");
-                let td_link = document.createElement("td");
-                td_model.innerHTML = oreditip(data[i].Model);
-                td_model.classList.add('text-center');
+                let td_akcije = document.createElement("td");
+                td_kategorija.innerHTML = oreditip(data[i].Model);
 
-                td_opis.innerHTML = data[i].Opis;
-                td_opis.classList.add('text-center');
-                td_cena.innerHTML = data[i].Cena;
-                td_cena.classList.add('text-center');
-                td_slika.innerHTML = data[i].Naziv;
-                td_slika.classList.add('text-center');
-                tr.appendChild(td_slika);
-                tr.appendChild(td_model);
+                tr.appendChild(td_kategorija);
                 tr.appendChild(td_opis);
                 tr.appendChild(td_cena);
+                tr.appendChild(td_akcije);
 
-                td_link.classList.add('text-center');
-                var btn = document.createElement('button');
+                let btn = document.createElement('button');
                 btn.type = "button";
                 btn.classList.add("btn");
                 btn.classList.add("btn-color");
                 btn.innerHTML = "Promena Cene";
-                td_link.appendChild(btn);
-                td_link.appendChild(this.document.createElement('br'));
-                var link = this.document.createElement('a');
+                td_akcije.appendChild(btn);
+                td_akcije.appendChild(this.document.createElement('br'));
+                let link = this.document.createElement('a');
                 link.setAttribute('href', 'jela.html');
                 link.classList.add('btn');
                 link.classList.add('btn-secondary');
                 link.innerHTML = 'Izmeni';
-                td_link.appendChild(link);
+                td_akcije.appendChild(link);
 
-                tr.appendChild(td_link);
+                tr.appendChild(td_akcije);
                 document.getElementById("spisak").appendChild(tr);
             }
 
