3454adff625cd25d920b11d0487ba3815b8e2389
DusanPajic002 <dusanpajic2002@gmail.com>
2023-11-02 09:45:24 +0100

    1

--- app_servis/static/jela.js
+++ app_servis/static/jela.js
@@ -1,26 +1,12 @@
 window.addEventListener("load", function(){
-	fetch('/jela')
+    fetch('/jela')
     .then(response => {
         let data = response.json(); 
         console.log(data);
         //...ovde ide dalja obrada odgovora
     })
-    for(let i=0; i<data.length; i++){
-        let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
-        td1.innerHTML = data[i].naziv;
-        tr.appendChild(td1);
-        //… ostatak
-        document.getElementById("spisak").appendChild(tr);
-        //data[i].kategorija
-        //data[i].cena
-        let btn = document.createElement("button");
-        btn.addEventListener("click", function(){
-    });
-    }
-    
-
     .catch(error => {
         console.error('Error:', error);
     });
+
 });
