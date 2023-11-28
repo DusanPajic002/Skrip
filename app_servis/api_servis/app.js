cd2cb34aa6e49eb286c6fbc152b8f8eb1277ebfe
DusanPajic002 <dusanpajic2002@gmail.com>
2023-11-28 23:48:26 +0100

    2

--- app_servis/api_servis/app.js
+++ app_servis/api_servis/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
+const jeloRoutes = require("C:/Users/Korisnik/Desktop/Skrip/app_servis/routes/jela.js");
+app.use("/jelo", jeloRoutes);
 app.get('/', (req, res) => { res.send('Hello from REST API service')});
 app.listen(9000);
